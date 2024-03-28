646efaeb42ff2e0e5db948053837b9b09639573d
Suteekarn016 <165233261+Suteekarn016@users.noreply.github.com>
2024-03-28 11:29:16 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 st.subheader("	✨Suteekarn	✨")
 st.image('images.jpg')
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
