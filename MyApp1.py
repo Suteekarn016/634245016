e94db108422cb0d64ce465848ebba2212950452c
Suteekarn016 <165233261+Suteekarn016@users.noreply.github.com>
2024-03-28 11:21:58 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
-st.subheader("Suteekarn")
+st.subheader("	✨Suteekarn	✨")
 st.image('images.jpg')
